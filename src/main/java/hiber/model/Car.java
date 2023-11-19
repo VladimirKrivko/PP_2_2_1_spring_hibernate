f7945bb249c0b3b8eb6d0cdc10d6c2c920d349fa
VladimirKrivko <mark.tatyschev@gmail.com>
2023-11-19 23:38:48 +0300

    Created the Car entity. Added this class to the hibernate settings. Created several users with machines and added them to the database. Added a method that gets a user who owns a car by its model and series.

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,78 @@
+package hiber.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return getId().equals(car.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+               "id=" + id +
+               ", model='" + model + '\'' +
+               ", series=" + series +
+               '}';
+    }
+}
--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -3,8 +3,13 @@ package hiber.service;
 import hiber.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
+
     void add(User user);
+
     List<User> listUsers();
+
+    Optional<User> getUserByModelAndSeries(String model, int series);
 }
